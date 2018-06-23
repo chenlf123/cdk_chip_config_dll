6718dfb919be6cea618f0f721bc73e4319a1ba77
Chen Linfei <linfei_chen@c-sky.com>
2018-06-23 20:42:33 +0800

    update chip_config_dll.cpp

--- vs_proj/chip_config_dll/chip_config_dll.cpp
+++ vs_proj/chip_config_dll/chip_config_dll.cpp
@@ -27,7 +27,7 @@ wxUint8 *json_content;
 wxFile file;
 wxFile log_file;
 
-#define LOG_FILE "/debug.log"
+#define LOG_FILE "debug.log"
 
 const wxString PIN_NUM = wxT("pin_num");
 const wxString PIN_SELECT = wxT("pin_select");
@@ -58,7 +58,7 @@ wxString thisDllDirPath()
                             (LPWSTR) &thisDllDirPath, &hm ) )
     {
         GetModuleFileNameW( hm, path, sizeof(path) );
-		thisPath = wxFileName(path).GetPath();
+        thisPath = wxFileName(path).GetPath();
         //PathRemoveFileSpecW( path );
         //thisPath = CStringW( path );
         //if( !thisPath.IsEmpty() && 
@@ -75,27 +75,27 @@ typedef std::shared_ptr<wxString> wxStringPtr;
 std::vector<JsonRootPtr> jsons;
 std::vector<wxStringPtr> jsonStrs;
 extern "C" {
-	int pin_num_file_init(void)
-	{
-		file.Open(thisDllDirPath() + wxT("/pin_name.json"), wxFile::read);
-		if (!(file.IsOpened())) {
-			return -1;
-		}
+    int pin_num_file_init(void)
+    {
+        file.Open(thisDllDirPath() + wxT("/pin_name.json"), wxFile::read);
+        if (!(file.IsOpened())) {
+            return -1;
+        }
 
-		file.Seek(0);
+        file.Seek(0);
 
-		wxFileOffset filesize = file.Length();
-		json_content = new wxUint8[filesize];
+        wxFileOffset filesize = file.Length();
+        json_content = new wxUint8[filesize];
 
-		if (file.Read((void *)json_content, filesize) != filesize) {
-			file.Close();
-			delete[] json_content;
-			return -1;
-		}
-		return 0;
-	}
-	int map_pin_fun(wxString pinNum, wxString select, wxString *pin_num, wxString *pin_func)
-	{
+        if (file.Read((void *)json_content, filesize) != filesize) {
+            file.Close();
+            delete[] json_content;
+            return -1;
+        }
+        return 0;
+    }
+    int map_pin_fun(wxString pinNum, wxString select, wxString *pin_num, wxString *pin_func)
+    {
 
         wxString input(json_content, wxConvUTF8);
         JSONRoot jsonPinInfo(input);
@@ -103,7 +103,7 @@ extern "C" {
         JSONElement pin = rootPin.firstChild();
         wxString PinNum, PinSelect, PinFunc;
 
-		while (pin.isOk()) {
+        while (pin.isOk()) {
             JSONElement property = pin.firstChild();
             while (property.isOk()) {
                 if (property.getName() == PIN_NUM) {
@@ -117,19 +117,24 @@ extern "C" {
                 }
                 property = pin.nextChild();
             }
-			
-			if (pinNum == PinNum) {
-				if (select == PinSelect) {
-					*pin_num = PinFunc.BeforeFirst('_');
-					*pin_func = PinFunc;
-					return 0;
-				}
-			}
+
+            wxString log = "pinNum: " + pinNum + "\n";
+            log << "PinNum:" + PinNum + "\n";
+            if (pinNum == PinNum) {
+                if (select == PinSelect) {
+                    log << "select2: " + select + "\n";
+                    log << "PinSelect2:" + PinSelect + "\n";
+                    log_file.Write(log);
+                    *pin_num = PinFunc.BeforeFirst('_');
+                    *pin_func = PinFunc;
+                    return 0;
+                }
+            }
 
             pin = rootPin.nextChild();
         }
-		return -1;
-	}
+        return -1;
+    }
     CHIP_CONFIG_DLL_API void OnInit() 
     {
         //do nothing
@@ -143,26 +148,24 @@ extern "C" {
     {
         return "there is no error message.";
     }
-	CHIP_CONFIG_DLL_API void pin_init_code(void)
-	{
-	}
+    CHIP_CONFIG_DLL_API void pin_init_code(void)
+    {
+    }
     CHIP_CONFIG_DLL_API char * pin_init_code_gen(char* jsonStr)
     {
-		pin_num_file_init();
+        pin_num_file_init();
         wxString input(jsonStr, wxConvUTF8);
         JSONRoot jsonPinInfo(input);
         JSONElement rootPin = jsonPinInfo.toElement();
         JSONElement pin = rootPin.firstChild();
         wxString pinNum, select, direct, pullup, dubounce;
-		wxString pin_num, pin_func;
-        wxString funcBody = "\n";
+        wxString pin_num, pin_func;
+        wxString funcBody = "";
 
-
-		int ret =log_file.Create(thisDllDirPath() + wxT(LOG_FILE),true);
-		log_file.Open(thisDllDirPath() + wxT(LOG_FILE), wxFile::write);
-		wxString log = "???\n!!!";
-		log_file.Write(log);
-		log_file.Close();
+        int ret =log_file.Create(thisDllDirPath() + wxT(LOG_FILE),true);
+        log_file.Open(thisDllDirPath() + "/" + wxT(LOG_FILE), wxFile::write);
+        wxString log = "???\n!!!\n";
+        log_file.Write(log);
 
         while (pin.isOk()) {
             JSONElement property = pin.firstChild();
@@ -185,79 +188,29 @@ extern "C" {
 
                 property = pin.nextChild();
             }
-			if (map_pin_fun(pinNum, select, &pin_num, &pin_func) != 0) {
-				pin = rootPin.nextChild();
-				continue;
-			}
-             //funcBody << "\tuser config is :\n";
-            // funcBody << "\t\tpinNum : " + pinNum + "\n";
-             //funcBody << "\t\tselect : " + select + "\n";
-             //funcBody << "\t\tdirect : " + direct + "\n";
-            // funcBody << "\t\tpullup : " + pullup + "\n";
-			 //funcBody << "\t\str_size : " + str_size + "\n";
-			 funcBody << wxT("\tdrv_pinmux_config(") + pin_num + wxT(", ") + pin_func + wxT(");");
-			 funcBody << "\n";
+            if (map_pin_fun(pinNum, select, &pin_num, &pin_func) != 0) {
+                pin = rootPin.nextChild();
+                continue;
+            }
+             funcBody << wxT("\tdrv_pinmux_config(") + pin_num + wxT(", ") + pin_func + wxT(");");
+             funcBody << "\n";
             pin = rootPin.nextChild();
         }
-//		funcBody << thisDllDirPath();
+//        funcBody << thisDllDirPath();
 
-			// prepare to generate the code
+        // prepare to generate the code
         JSONRoot *root = new JSONRoot(cJSON_Object);
         wxString funcName = "pinmux_init";
         root->toElement().addProperty(funcName, funcBody);
         jsons.push_back(JsonRootPtr(root));
         wxString *jstr = new wxString(root->toElement().format());
         jsonStrs.push_back(wxStringPtr(jstr));
-		file.Close();
-		delete[] json_content;
+        file.Close();
+        delete[] json_content;
 
-        return (char*)jstr->mb_str(wxConvUTF8).data();
-    }
+        /* Close Log File */
+        log_file.Close();
 
-    CHIP_CONFIG_DLL_API char * UART0_init_code_gen(char* jsonStr)
-    {
-        wxString input(jsonStr, wxConvUTF8);
-        JSONRoot jsonDevInfo(input);
-        JSONElement rootDev = jsonDevInfo.toElement();
-        JSONElement dev = rootDev.firstChild();
-        wxString baud, mode, parity, stop_bit, uart_bit;
-        while (dev.isOk()) {
-            if (dev.getName() == BAUD) {
-                baud = dev.toString();
-            }
-            else if (dev.getName() == MODE) {
-                mode = dev.toString();
-            }
-            else if (dev.getName() == PARITY) {
-                parity = dev.toString();
-            }
-            else if (dev.getName() == STOP_BIT) {
-                stop_bit = dev.toString();
-            }
-            else if (dev.getName() == UART_BIT) {
-                uart_bit = dev.toString();
-            }
-
-            dev = rootDev.nextChild();
-        }
-
-        JSONRoot *root = new JSONRoot(cJSON_Object);
-        wxString funcName = "uart0_init";
-        wxString funcBody = "/**";
-        funcBody << "\tuser config is :\n";
-        funcBody << "\t\tbaud : " + baud + "\n";
-        funcBody << "\t\tmode : " + mode + "\n";
-        funcBody << "\t\tparity : " + parity + "\n";
-        funcBody << "\t\tstop_bit : " + stop_bit + "\n";
-        funcBody << "\t\tuart_bit : " + uart_bit + "\n";
-        funcBody << "*/\n";
-		//funcBody << wxT("drv_config(") + baud + wxT(", ") + mode + wxT(");";
-		//funcBody << wxT("drv_config(") + baud + wxT(", ") + mode + wxT(")");
-		funcBody << "\n";
-        root->toElement().addProperty(funcName, funcBody);
-        jsons.push_back(JsonRootPtr(root));
-        wxString *jstr = new wxString(root->toElement().format());
-        jsonStrs.push_back(wxStringPtr(jstr));
         return (char*)jstr->mb_str(wxConvUTF8).data();
     }
 }
